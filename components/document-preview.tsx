8149928df4630b28a61b7aada4646934a1ba3d25
Amirkabbara <amirkabbara@gmail.com>
2025-05-21 07:02:17 +0300

    fixed build issue with document preview

--- components/document-preview.tsx
+++ components/document-preview.tsx
@@ -2,16 +2,16 @@
 
 import {
   memo,
-  MouseEvent,
   useCallback,
   useEffect,
   useMemo,
   useRef,
+  type MouseEvent,
 } from 'react';
-import { ArtifactKind, UIArtifact } from './artifact';
+import type { ArtifactKind, UIArtifact } from './artifact';
 import { FileIcon, FullscreenIcon, ImageIcon, LoaderIcon } from './icons';
 import { cn, fetcher } from '@/lib/utils';
-import { Document } from '@/lib/db/schema';
+import type { Document } from '@/lib/db/schema';
 import { InlineDocumentSkeleton } from './document-skeleton';
 import useSWR from 'swr';
 import { Editor } from './text-editor';
@@ -94,6 +94,8 @@ export function DocumentPreview({
           id: artifact.documentId,
           createdAt: new Date(),
           userId: 'noop',
+          is_latest: true,
+          version: '1',
         }
       : null;
 
